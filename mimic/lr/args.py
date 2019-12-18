4daa1dcc8c09ebdcf87a6407e6e67877098a703f
Sudarshan <su0@ornl.gov>
2019-12-18 14:49:44 -0500

    tarted lr

---
+++ mimic/lr/args.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import pickle
+from argparse import Namespace
+from pathlib import Path
+
+path = Path('../data')
+workdir = path/f'workdir/lr'
+
+args = Namespace(
+  path=path,
+  workdir=workdir,
+  figdir=workdir/'figures',
+  structured_csv=path/'modelready_structured.csv',
+  unstructured_csv=path/'modelready_unstructured.csv',
+  mm_csv=path/'modelready_mm.csv',
+  modeldir=workdir/'models',
+)
+
+# notes_common_vital_threshold=0.41,
+# full_common_vital_threshold=0.43,
+# notes_common_all_threshold=0.46,
+# full_common_all_threshold=0.5,
