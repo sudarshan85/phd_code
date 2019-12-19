38efc100236f36e1d36e1f7754848f49f68f35e1
Sudarshan <su0@ornl.gov>
2019-12-19 11:44:00 -0500

    GBM start

---
+++ mimic/gbm/args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import pickle
+from argparse import Namespace
+from pathlib import Path
+
+path = Path('../data')
+workdir = path/f'workdir/gbm'
+
+args = Namespace(
+  path=path,
+  workdir=workdir,
+  figdir=path/'workdir/figures',
+  dataset_csv=path/'modelready_mm.csv',
+  modeldir=workdir/'models',
+  vectordir=path/'workdir/vectordir',
+  str_cols_pkl=path/'str_cols.pkl',
+  structured_threshold=0.33,
+  unstructured_threshold=0.334,
+  mm_threshold=0.326,
+)
