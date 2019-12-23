59dd05b0f8f0cfb56bfb7d225433793f914b9c67
Sudarshan Srinivasan <sudarshan85@gmail.com>
2019-12-23 12:43:18 -0500

    LM

--- mlh/lr/args.py
+++ mlh/lr/args.py
@@ -4,12 +4,13 @@ from argparse import Namespace
 from pathlib import Path
 
 path = Path('../data')
-workdir = path/'workdir'
+workdir = path/'workdir/lr'
 
 args = Namespace(
   path=path,
   workdir=workdir,
-  figdir=path/'figures',
+  figdir=path/'workdir/figures',
+  vectordir=path/'workdir/vectordir',
   dataset_csv=path/'unstructured_proc.csv',
   threshold=0.31,
   )
